6c493f20ceeb6249fd901b669adf5f6527c47492
aditya-ch-cloud <adityach151@gmail.com>
2023-09-29 19:12:51 +0530

    every thing working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,6 +68,7 @@ const Header = () => {
                         <IoIosSearch className="text-white text-xl" />
                     </div>
                     <input
+                        
                         type="text"
                         className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
                         onChange={(e) => { setSearchQuery(e.target.value) }}
@@ -75,7 +76,10 @@ const Header = () => {
                         value={searchQuery}
                     />
                 </div>
-                <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]">
+                <button 
+                    className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]"
+                    onClick={() => {searchQueryHandler("searchButton")}}
+                >
                     <IoIosSearch className="text-white text-xl" />
                 </button>
             </div>
@@ -87,9 +91,9 @@ const Header = () => {
                     <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
                         <FiBell className="text-white text-xl cursor-pointer"/>
                     </div>
-                    <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-                        <img src="https://api.multiavatar.com/Binx Bond.svg" alt="ProfilePicture" />
-                    </div>
+                </div>
+                <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
+                    <img src="https://api.multiavatar.com/Binx Bond.svg" alt="ProfilePicture" />
                 </div>
             </div>
         </div>
