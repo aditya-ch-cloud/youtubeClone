179df9746b278471c1ff67449f64397f0a990b35
aditya-ch-cloud <adityach151@gmail.com>
2023-10-30 17:53:18 +0530

    fixed issues with light mode themes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
     const { pathname } = useLocation();
     const pageName = pathname?.split("/")?.filter(Boolean)?.[0]
     return (
-        <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+        <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-black">
             {loading && <Loader />}
 
             <div className="flex h-5 items-center">
@@ -51,7 +51,7 @@ const Header = () => {
                 )}
                 <Link to="/" className="flex h-5 items-center">
                     <img 
-                        className="h-full hidden dark:md:block" 
+                        className="h-full md:block" 
                         src={ytLogo} 
                         alt='YouTube' 
                     />
